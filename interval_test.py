97b9b085ca7360227bcda0ef0d85fe1342ba03db
John Richard Perez <46211205+Junorium@users.noreply.github.com>
2024-10-02 13:48:18 -0400

    Update interval_test.py

--- interval_test.py
+++ interval_test.py
@@ -1,1 +1,15 @@
+import numpy as np
+scales_sharp = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
 
+def main():
+  while True:
+    game_mode = input('Type of Game Mode: for scale, Random or Given?')
+    if game_mode.lower().strip() == 'random':
+      scale = scales_sharp[np.random.randint(0, 12)]
+      play(scale)
+    elif game_mode.lower().strip() == 'given':
+      scale = input('Scale?')
+      try:
+        play(scales_sharp[])
+      except:
+        play(scales_flat[])
